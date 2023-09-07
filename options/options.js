0d06222147f7bab268b0e4915c440ccd52127b71
Crowdbotics <team@crowdbotics.com>
2023-09-07 10:20:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "arplo-brain-express-43263",
+  url: "https://arplo-brain-express-43263.botics.co",
+  api: "https://arplo-brain-express-43263.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
